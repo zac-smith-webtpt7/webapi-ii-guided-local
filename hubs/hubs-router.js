8513d4fa66390f28bb2063ff98e25dcc30cd94e3
Zac Smith <zrs3141592@gmail.com>
2019-10-29 11:34:00 -0600

    practice users

---
+++ hubs/hubs-router.js
@@ -0,0 +1,97 @@
+const router = require("express").Router();
+
+const Hubs = require("./hubs-model.js");
+
+router.get("", (req, res) => {
+  Hubs.find(req.query)
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the hubs"
+      });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Hubs.findById(req.params.id)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: "Hub not found" });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the hub"
+      });
+    });
+});
+
+router.post("", (req, res) => {
+  Hubs.add(req.body)
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the hub"
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Hubs.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "The hub has been nuked" });
+      } else {
+        res.status(404).json({ message: "The hub could not be found" });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the hub"
+      });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  Hubs.update(req.params.id, changes)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: "The hub could not be found" });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating the hub"
+      });
+    });
+});
+
+// add an endpoint that returns all the messages for a hub
+router.get("/:id/messages", (req, res) => {});
+router.post("/:id/messages", (req, res) => {
+  const hubId = req.params.id;
+  const message = { ...req.body, hubId: req.params.id };
+});
+router.get("/:id/users", (req, res) => {});
+// add an endpoint for adding new message to a hub
+
+module.exports = router;
---
+++ users/users-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+const Users = require("./users-model.js");
+
+router.get("/", (req, res) => {});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
